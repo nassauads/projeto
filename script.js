3bf59d84b4d94592f5b34d4e806132e0eecd44d9
nassauads <custotech24@gmail.com>
2024-09-23 12:30:17 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+// Função para fechar todos os cards abertos
+function fecharTodosOsCards() {
+    document.querySelectorAll('.flip-card').forEach(card => {
+        card.classList.remove('flip');
+    });
+}
+
+// Adiciona evento de flip ao clicar no botão "Clique-me" e ao clicar no card
+document.querySelectorAll('.flip-card').forEach(card => {
+    card.addEventListener('click', function (event) {
+        event.stopPropagation(); // Previne a propagação do clique
+        if (!this.classList.contains('flip')) { // Verifica se o card já está aberto
+            fecharTodosOsCards(); // Fecha todos os cards abertos
+            this.classList.add('flip'); // Abre o card atual
+        }
+    });
+});
+
+// Adiciona evento para fechar o flip ao clicar no botão "X"
+document.querySelectorAll('.close-button').forEach(button => {
+    button.addEventListener('click', function (event) {
+        event.stopPropagation();
+        this.closest('.flip-card').classList.remove('flip');
+    });
+});
+
 function redirecionar(url) {
     window.location.href = url;
 }
